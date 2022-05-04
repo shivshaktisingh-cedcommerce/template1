2582e3cefc4e3eaf9cad2ba43e7a978b01b39626
shivshaktisingh-cedcommerce <shivshaktisingh@cedcoss.com>
2022-05-04 10:49:57 +0530

    calculator

---
+++ calculator.js
@@ -0,0 +1,31 @@
+
+
+
+function addition(){
+    var num1 = parseInt(document.getElementById("fnum").value);
+    var num2 = parseInt(document.getElementById("lnum").value);
+    document.getElementById("result").innerHTML = num1 + num2; 
+}
+function subtraction(){
+    
+    var num1 = parseInt(document.getElementById("fnum").value);
+    var num2 = parseInt(document.getElementById("lnum").value); 
+    document.getElementById("result").innerHTML = num1 - num2; 
+}
+function multiplication(){
+    var num1 = parseInt(document.getElementById("fnum").value);
+    var num2 = parseInt(document.getElementById("lnum").value);
+    document.getElementById("result").innerHTML = num1 * num2; 
+}
+function division(){
+    var num1 = parseInt(document.getElementById("fnum").value);
+    var num2 = parseInt(document.getElementById("lnum").value); 
+    document.getElementById("result").innerHTML = num1 / num2; 
+}
+
+
+
+
+
+
+
